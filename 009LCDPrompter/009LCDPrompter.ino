6c3941bbee5326b6cd34e7a45421c1e16eb24f6d
Abdurrahman Aygun <139715066+aayggunn@users.noreply.github.com>
2025-05-04 00:47:50 +0300

    LCDPrompter

---
+++ 009LCDPrompter/009LCDPrompter.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+String message = "  286 ERCIYES UNIVERSITESI - SEHIR HASTANESI ZIYAGOKALP  ";
+int position = 0;
+
+void setup() {
+  lcd.begin(16, 2); // 16x2 LCD ekran
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print(message.substring(position, position + 16));
+
+  position++;
+  if (position > message.length() - 16) {
+    position = 0;
+  }
+
+  delay(500); // Kayma hız
+}
