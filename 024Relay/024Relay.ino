6dce0896f276d2dd39425610e9baad8aceb1c826
Abdurrahman Aygun <139715066+aayggunn@users.noreply.github.com>
2025-05-08 14:41:13 +0300

    relay

---
+++ 024Relay/024Relay.ino
@@ -0,0 +1,16 @@
+int relayPin=7;
+
+void setup() {
+
+  pinMode(relayPin, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(relayPin, HIGH);
+  delay(3000);
+  digitalWrite(relayPin, LOW);
+  delay(3000);
+  
+}
